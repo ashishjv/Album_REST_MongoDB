eb0000a9e8e024d9985439a6e999a85f0087fcb2
Ashish Joseph Vattoth <12287103+ashishjv@users.noreply.github.com>
2020-11-04 19:50:24 -0600

    Just from https://start.spring.io/

---
+++ src/main/java/com/example/album_REST_MongoDB/AlbumRestMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.album_REST_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumRestMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumRestMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/album_REST_MongoDB/AlbumRestMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.album_REST_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumRestMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
